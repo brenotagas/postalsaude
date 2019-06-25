3f701aab3680dd9f36c6908e7a0fc3f54872878e
Denner Ribeiro Bacelar <denner.bacelar@postalsaude.com.br>
2019-06-25 15:23:15 -0300

    Layout Aposentados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import { RotinaAtualizacaoService } from './services/rotina-atualizacao.service';
+import { HttpClientModule } from '@angular/common/http';
+
+import { registerLocaleData } from '@angular/common';
+import localeBr from '@angular/common/locales/pt';
+import { DrillComponent } from './components/drill/drill.component';
+import { RegrasComponent } from './components/regras/regras.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DrillComponent,
+    RegrasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [RotinaAtualizacaoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+registerLocaleData(localeBr, 'pt');
