dc56e03c3b8b43288f1cf79fd1a1d48571b67963
Denner Ribeiro Bacelar <denner.bacelar@postalsaude.com.br>
2019-07-19 17:41:34 -0300

    Pipe nome implementado e finalizado

--- src/app/pipes/nome.pipe.ts
+++ src/app/pipes/nome.pipe.ts
@@ -10,24 +10,17 @@ export class NomePipe implements PipeTransform {
   }
 
   transform(value: any, ...args: any[]): any {
-
-    // var nome = value.replace(/ Da | Das | De | Do | Dos /gi, function (x) {
-    //   x.toLowerCase();    
-    // });
-    // return nome;
-
     let prep = ['Da', 'Das', 'De', 'Do', 'Dos'];
     let array = value.split(" ");
-
     for (let i = 0; i < array.length; i++) {
       for (let j = 0; j < prep.length; j++) {
         const elementPrep = prep[j];
         const elementArray = array[i];
         if (elementArray == elementPrep) {
-          let index = value.indexOf(elementArray)
-          //console.log("aqui");
-          value.replace(/ Da | Das | De | Do | Dos /gi, function (x) {x.toLowerCase();});
-          return value;
+          var nome = value.replace(/ Da | Das | De | Do | Dos /gi, function (x) {
+            return x.toLowerCase();
+          });
+          return nome;
         }
       }
     }
