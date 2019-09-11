8b6ee19b31cdbbc7b5477e4ed5cbc3335fb75053
Denner Ribeiro Bacelar <denner.bacelar@postalsaude.com.br>
2019-09-11 13:11:10 -0300

    Inclusão do Google Analytics

--- src/app/pages/homeredirect/homeredirect.component.ts
+++ src/app/pages/homeredirect/homeredirect.component.ts
@@ -51,16 +51,15 @@ export class HomeredirectComponent implements OnInit {
     matriculaTratada = matriculaTratada.trim();
     this.rotinaservice.BuscarInformacoesPorMatricula(matriculaTratada).subscribe((res: any) => {
       this.dados = res.data;
-      //console.log(this.dados);
       if (this.dados.matricula == null && this.dados.nome == null && this.dados.tipoBeneficiario == null) {
         window.location.href='http://www.postalsaude.com.br/';
       } else if (this.dados.tipoBeneficiario == "Aposentado Normal" || this.dados.tipoBeneficiario == "Aposentado por Invalidez") {
         this.router.navigate(['drill-aposentados'], { queryParams: { matricula: matriculaTratada } })
-      } else if (this.dados.tipoBeneficiario == "Ativo" || this.dados.tipoBeneficiario == "Afastado") {
+      } else if (this.dados.tipoBeneficiario == "Ativo") {
         this.router.navigate(['drill-ativos'], { queryParams: { matricula: matriculaTratada } })
-      }// } else {
-      //   this.router.navigate(['drill-afastados'], { queryParams: { matricula: matriculaTratada } })
-      // }
+      } else {
+        this.router.navigate(['drill-afastados'], { queryParams: { matricula: matriculaTratada } })
+      }
     },
       error => {
         console.log(error.error);
