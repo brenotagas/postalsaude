65051ccb9d9a8938371859adbcb2685233e80cce
Denner Ribeiro Bacelar <denner.bacelar@postalsaude.com.br>
2019-09-12 10:28:07 -0300

    Botão "Nova Consulta" retirado

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  mostrar: boolean = true;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    //this.VerificarQueryString();
+  }
+
+  VerificarQueryString(): any {
+    this.route.queryParams.subscribe(params => {
+      //console.log(params.matricula)
+      params.source;
+      if (params.source) {
+        this.mostrar = false;
+      }
+    });
   }
 
 }
